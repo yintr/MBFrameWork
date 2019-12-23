fc099f0fcca0a25dde64633bcc89e7353c70e13b
yintr <yintisi2009@sina.com>
2019-12-23 16:23:32 +0800

    Release 0.1.0

---
+++ Example/Pods/Target Support Files/MBFrameWork/MBFrameWork-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MBFrameWork : NSObject
+@end
+@implementation PodsDummy_MBFrameWork
+@end
---
+++ Example/Pods/Target Support Files/Pods-MBFrameWork_Tests/Pods-MBFrameWork_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MBFrameWork_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_MBFrameWork_Tests
+@end
